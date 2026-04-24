deef7b0f7040d9c92bdcb1c0ce36228b156b4aba
dbszl3 <yoonji0811@naver.com>
2026-04-24 13:36:25 +0900

    ✨ feat: 정렬 버튼 및 정렬 모달 기능 추가

--- src/components/sort/SortButton.jsx
+++ src/components/sort/SortButton.jsx
@@ -1,33 +1,58 @@
+import { useState } from "react";
 import styled from "styled-components";
 import sortIcon from "../../assets/icons/SortIcon.png";
+import SortModal from "./SortModal";
 
-const FilterContainer = styled.div`
-    display: inline-flex;
-    align-items: center;
-    gap: 7px;
+const SortWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const FilterContainer = styled.button`
+  border: none;
+  background: transparent;
+  padding: 0;
+  display: inline-flex;
+  align-items: center;
+  gap: 7px;
+  cursor: pointer;
 `;
 
 const Sort = styled.p`
-    color: #616161;
-    font-family: Pretendard;
-    font-size: 13px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
+  color: #616161;
+  font-family: Pretendard;
+  font-size: 13px;
+  font-weight: 400;
+  margin: 0;
 `;
 
 const SortIcon = styled.img`
-    width: 10px;
-    height: 11px;
-    stroke-width: 1px;
-    stroke: #909090;
-    display: block;
+  width: 10px;
+  height: 11px;
+  display: block;
 `;
 
 export default function SortButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedSort, setSelectedSort] = useState("기본 정렬순");
+
   return (
-    <FilterContainer>
-      <Sort>정렬순</Sort><SortIcon src={sortIcon} alt="정렬 아이콘" />
-    </FilterContainer>
+    <SortWrapper>
+      <FilterContainer onClick={() => setIsOpen((prev) => !prev)}>
+        <Sort>정렬순</Sort><SortIcon src={sortIcon} />
+      </FilterContainer>
+
+      {isOpen && (
+        <SortModal
+          selectedSort={selectedSort}
+          onSelect={(option) => {
+            setSelectedSort(option);
+            setIsOpen(false);
+          }}
+        />
+      )}
+    </SortWrapper>
   );
 }
