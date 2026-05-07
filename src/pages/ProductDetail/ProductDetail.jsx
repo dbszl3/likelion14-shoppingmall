89c35aeeef93d19ff7b61cead98fcaf47f079f86
dbszl3 <yoonji0811@naver.com>
2026-05-07 23:36:40 +0900

    ✨ Feat: 상품 상세조회 페이지 구현

---
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,137 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import productDummy from "../Main/productDummy";
+import star from "../../assets/icons/Star.png";
+
+const displayProducts = [...productDummy, ...productDummy].map((item, index) => ({
+  ...item,
+  id: index + 1,
+}));
+
+const DetailContainer = styled.div`
+  display: flex;
+`;
+
+const ProductImage = styled.img`
+  width: 459px;
+  height: 602px;
+  aspect-ratio: 61/80;
+  margin-left: 204px;
+  margin-top: 70px;
+`;
+
+const Line = styled.div`
+  width: 1.5px;
+  height: 760px;
+  background: #EBEBEB;
+  justify-content: center;
+  align-items: center;
+  margin-left: 146.01px;
+`;
+
+const InfoSection = styled.div`
+  display: flex;
+  width: 247px;
+  height: 107px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 62.99px;
+  margin-top: 124px;
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+`;
+
+const Price = styled.p`
+  color: #000;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #000;
+  font-family: Pretendard;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const ProductName = styled.p`
+  color: #333;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const ReviewRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 8px;
+`;
+
+const Rating = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+
+const Star = styled.img`
+  width: 13px;
+  height: 12px;
+  aspect-ratio: 13/12;
+`;
+
+const Rate = styled.p`
+  color: #333;
+  font-family: Pretendard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const Review = styled.p`
+  color: #949494;
+  font-family: Pretendard;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export default function ProductDetail() {
+  const { id } = useParams();
+  const productId = Number(id);
+  const product = displayProducts.find((item) => item.id === productId);
+
+  return (
+    <DetailContainer>
+      
+        <ProductImage src={product.imageUrl} alt={product.name} />
+        
+        <Line />
+
+        <InfoSection>
+          <InfoBox>
+            <Price>{product.price.toLocaleString()}원</Price>
+
+            <div>
+              <ProductName>{product.name}</ProductName>
+
+              <ReviewRow>
+                <Rating>
+                  <Star src={star} />
+                  <Rate>{product.rating}</Rate>
+                </Rating>
+                <Review>리뷰 {product.reviewCount.toLocaleString()}</Review>
+              </ReviewRow>
+            </div>
+          </InfoBox>
+        </InfoSection>
+    </DetailContainer>
+  );
+}
